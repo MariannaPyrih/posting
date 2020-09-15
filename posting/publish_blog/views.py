6c429e8df4fde3a00e3c6d74bc0cfa8b86b9a49a
MariannaPyrih <mariannapyrih@gmail.com>
2020-09-15 12:35:00 +0300

    ///

---
+++ posting/publish_blog/views.py
@@ -0,0 +1,10 @@
+
+#publish_blog /views.py
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'index.html', context=None)
+
+# Створіть свої представлення тут. class HomePageView(TemplateView):     def get(self, request, **kwargs):         return render(request, ‘index.html’, context=None)
